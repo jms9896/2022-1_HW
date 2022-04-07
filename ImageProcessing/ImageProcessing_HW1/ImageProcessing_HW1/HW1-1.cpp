1af4aad97f2f3466d3a863e104287076f8f21f81
jms9896 <jms9896@gmail.com>
2022-04-07 23:31:53 +0900

    영상처리 프로젝트 이름변경

---
+++ ImageProcessing/ImageProcessing_HW1/ImageProcessing_HW1/HW1-1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#pragma warning(disable : 4996)
+
+int main(void)
+{
+	int imageSize = 512;
+	int light = 0;
+	unsigned char image[512][512];
+
+	for (int i = 0; i < imageSize; i++) {
+		if (i < 100) {
+			light = 120;
+			for (int j = 0; j < imageSize; j++) {
+				image[i][j] = light;
+			}
+		}
+
+		else if (i < 200) {
+			light = (15 / (i - 100));
+			for (int j = 0; j < imageSize; j++) {
+				image[i][j] = light;
+			}
+		}
+
+		else if (i < 280) {
+			light = (225 - 135) / (i - 200);
+			for (int j = 0; j < imageSize; j++) {
+				image[i][j] = light;
+			}
+		}
+
+		else if (i < 300) {
+			light = 15 / (i - 300);
+			for (int j = 0; j < imageSize; j++) {
+				image[i][j] = light;
+			}
+		}
+
+		else {
+			light = 240;
+			for (int j = 0; j < imageSize; j++) {
+				image[i][j] = light;
+			}
+		}
+
+		//printf("%d", light);
+	}
+	for (int i = 0; i < 512; i++) {
+		printf("%d", image[0][i]);
+	}
+	//printf(image);
+	FILE* fp;
+	fp = fopen("HW1-1.raw", "wb");
+	fwrite(image, sizeof(unsigned char), imageSize * imageSize, fp);
+	fclose(fp);
+
+	return 0;
+}
