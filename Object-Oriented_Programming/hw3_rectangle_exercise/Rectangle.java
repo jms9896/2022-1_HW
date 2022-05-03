e9138615ee39878a2bd6e2134f7a6856c33775c4
jms9896 <jms9896@gmail.com>
2022-05-03 21:27:05 +0900

    OOP HW3_base

---
+++ Object-Oriented_Programming/hw3_rectangle_exercise/Rectangle.java
@@ -0,0 +1,28 @@
+/*
+ * Q: Write Rectangle class that implements the following features:
+ *
+ * 1. Make RectangleRunner.java run with Rectangle class that you design
+ *      in Rectangle.java
+ *      a. First of all, take a look at output_RectangleRunner.txt and
+ *          plan to make RectangleRunner.class generate the desired output
+ *      b. No need to modify RectangleRunner.java. Only write code lines
+ *          in Rectangle.java
+ *      c. Start by modifying the template in this file (Rectangle.java)
+ * 2. In particular, design Rectangle class applying the object design technique
+ *      of ADT (Abstract Data Type):
+ *      a. instance variables declared as private
+ *      b. constructors with width and height variables as initial input parameters
+ *      c. methods: calculateArea(), print(), resize()
+ *      d. setters and getters for the instance variables
+ */
+
+public class Rectangle
+{
+    /* instance variables go here (width and height) */
+
+    /* Constructors */
+
+    /* Methods: calculateArea(), print(), resize() */
+
+    /* Setters and getters */
+}
