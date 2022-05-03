25c0cbb8f30c90918dd4d1e7f571ebe62de059d2
jms9896 <jms9896@gmail.com>
2022-05-03 22:00:50 +0900

    버그수정완료 과제완료

--- Object-Oriented_Programming/HW3/src/Rectangle.java
+++ Object-Oriented_Programming/HW3/src/Rectangle.java
@@ -46,7 +46,7 @@ public class Rectangle
     public void print() {
         System.out.println(width + " * " + height);
         for (int i = 1; i <= height; i++){
-            for (int j = 1; j <= width; i++){
+            for (int j = 1; j <= width; j++){
                 System.out.print("*");
             }
             System.out.println();
